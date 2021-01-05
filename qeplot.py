388d354cd1a4387bfaaccae139e7e10829d5a12f
Lonitch <invictumltd@gmail.com>
2021-01-04 23:36:25 -0600

    updated dosplot and bndplot for results from soc calculations

--- qeplot.py
+++ qeplot.py
@@ -25,60 +25,69 @@ def Symmetries(fstring):
     return x
 
 # This function extracts the band data, and plots the bands, the fermi energy in red
-def bndplot(bdpath,scfpath,subplot,label=None,yrange=20):
+def bndplot(datafile=None,scffile=None,symmetryfile=None,bdpath=None,scfpath=None,
+            subplot=None,label=None,yrange=10,lw=1,unit='Ry',insanity=False):
     """
     Params:
+        datafile: path+filename for band data file (dat.gnu)
+        scffile: path +filename for the result of scf/nscf calc
+        symmfile: path +filename for output file of bands.x calc
+        ! using the following args if none of above is provided !
         bdpath: path to the results of bands calc, where you store `xx.dat.gnu` and `xx_band.out`,
                 for definitions of these two files, please check the comments in `raw2qe.py`.
         scfpath: path to the result of scf calc, we need this to extract fermi energy level info.
         subplot: an axis object for plotting band structure
         label: a title string for the plot
         yrange: set limit of y axis to [-yrange, yrange] in the unit of eV.
+        unit: if unit is set to be 'Ry', the code will convert energy values to 'eV'
+        insanity: indicator of abnormal values in bands results, if true, will start a sanity check
     """
-    datafiles = glob.glob(os.path.join(bdpath,'*dat.gnu'))
-    symmfiles = glob.glob(os.path.join(bdpath,'*band.out'))
-    scffiles = glob.glob(os.path.join(scfpath,'*.out'))
+    # If the file name is directly given, we skip the file search
+    if datafile is None or scffile is None or symmetryfile is None:
+        datafiles = glob.glob(os.path.join(bdpath,'*dat.gnu'))
+        symmfiles = glob.glob(os.path.join(bdpath,'*band.out'))
+        scffiles = glob.glob(os.path.join(scfpath,'*.out'))
+        #File search
+        # find data.gnu file
+        if len(datafiles)>1:
+            print('tell me which file you want using:')
+            for i,f in enumerate(datafiles):
+                print("{}. {}\n".format(i,f))
+            idx = int(input('Type it here:'))
+            datafile = datafiles[idx]
+        elif not datafiles:
+            print('There is no gnu data file!!!')
+            return -1
+        else:
+            datafile = datafiles[0]
 
-    # find data.gnu file
-    if len(datafiles)>1:
-        print('tell me which file you want using:')
-        for i,f in enumerate(datafiles):
-            print("{}. {}\n".format(i,f))
-        idx = int(input('Type it here:'))
-        datafile = datafiles[idx]
-    elif not datafiles:
-        print('There is no gnu data file!!!')
-        return -1
-    else:
-        datafile = datafiles[0]
+        # Find band.out file
+        if len(symmfiles)>1:
+            print('tell me which symmetry file you want using:')
+            for i,f in enumerate(symmfiles):
+                print("{}. {}\n".format(i,f))
+            idx = int(input('Type it here:'))
+            symmetryfile = symmfiles[idx]
+        elif not symmfiles:
+            print('There is no band data file!!!')
+            return -1
+        else:
+            symmetryfile = symmfiles[0]
 
-    # Find band.out file
-    if len(symmfiles)>1:
-        print('tell me which symmetry file you want using:')
-        for i,f in enumerate(symmfiles):
-            print("{}. {}\n".format(i,f))
-        idx = int(input('Type it here:'))
-        symmetryfile = symmfiles[idx]
-    elif not symmfiles:
-        print('There is no band data file!!!')
-        return -1
-    else:
-        symmetryfile = symmfiles[0]
+        # find scf/relax output file
+        if len(scffiles)>1:
+            print('tell me which file you want using:')
+            for i,f in enumerate(scffiles):
+                print("{}. {}\n".format(i,f))
+            idx = int(input('Type it here:'))
+            scffile = scffiles[idx]
+        elif not scffiles:
+            print('There is no scf output file!!!')
+            return -1
+        else:
+            scffile = scffiles[0]
 
-    # find scf/relax output file
-    if len(scffiles)>1:
-        print('tell me which file you want using:')
-        for i,f in enumerate(scffiles):
-            print("{}. {}\n".format(i,f))
-        idx = int(input('Type it here:'))
-        scffile = scffiles[idx]
-    elif not scffiles:
-        print('There is no scf output file!!!')
-        return -1
-    else:
-        scffile = scffiles[0]
-
-    # Plot title string
+    # Plot title string, could be reset outside of the function
     if label is None:
         label=os.path.split(scffile)[1][:-4]
 
@@ -99,9 +108,32 @@ def bndplot(bdpath,scfpath,subplot,label=None,yrange=20):
         sel = z[z[:,0] == x[i]]  #Here is the energies for a given x
         for j in range(0,bndl): #This separates it out into a single band
             bands[j][i][0] = x[i]
-            bands[j][i][1] = np.multiply(sel[j][1],13.605698066)
-    for i in bands: #Here we plots the bands
-        subplot.plot(i[:,0],i[:,1],color="black")
+            if unit=='Ry':
+                bands[j][i][1] = np.multiply(sel[j][1],13.605698066)
+            else:
+                bands[j][i][1] = sel[j][1]
+    if insanity:
+        from scipy import interpolate
+        newbands=[]
+        for i,band in enumerate(bands):
+            std=np.std(band[:,1])
+            avg = np.mean(band[:,1])
+            idx = [avg-2*std<_a<avg+2*std for _a in band[:,1]]
+            if sum(idx)>5:
+                band = band[idx]
+            tck = interpolate.splrep(band[:,0], band[:,1], s=0)
+            xnew = np.linspace(np.min(band[:,0])-0.5, np.max(band[:,0])+0.5, 1000)
+            ynew = interpolate.splev(xnew, tck)
+            temp = np.zeros((1000,2))
+            temp[:,0] = xnew
+            temp[:,1] = ynew
+            newbands.append(temp)
+        for i in newbands: #Here we plots the bands
+            subplot.plot(i[:,0],i[:,1],color="black",lw=lw)
+    else:
+        for i in bands: #Here we plots the bands
+            subplot.plot(i[:,0],i[:,1],color="black",lw=lw)
+
     temp = Symmetries(symmetryfile)
     for j in temp: #This is the high symmetry lines
         x1 = [j,j]
@@ -115,34 +147,38 @@ def bndplot(bdpath,scfpath,subplot,label=None,yrange=20):
     subplot.set_xlim([axis[0],axis[1]])
     subplot.set_title(label)
     subplot.set_ylabel('Energy(eV)')
+    return bands
 
 # The following two functions plot dos and pdos results
-def dosplot(dospath,ax,tag='_dos',xrange=15,lw=2):
+def dosplot(ax,dospath=None,datafile=None,tag='_dos',fermi=None,xrange=15,lw=2):
     """
     Params:
         dospath: a path to the folder that stores `xxx_dos.dat` file
+        datafile: path+filename to data file, if it is used, function will ignore `dospath`
         ax: axis object for plotting dos distribution
         tag: a substring in the file name, if multiple files with the same
              tag are find, the function will ask for clarification.
         label: title text for the plot
         xrange: set limits of x axis to be [-xrange,xrange]
     """
-    datafiles = glob.glob(os.path.join(dospath,'*{}.dat'.format(tag)))
-    if len(datafiles)>1:
-        print('tell me which file you want using:')
-        for i,f in enumerate(datafiles):
-            print("{}. {}\n".format(i,f))
-        idx = int(input('Type it here:'))
-        datafile = datafiles[idx]
-    elif not datafiles:
-        print('There is no gnu data file!!!')
-        return -1
-    else:
-        datafile = datafiles[0]
-    with open(datafile,'r') as f:
-        fermi = f.readlines()[0]
-    fermi = float(fermi.split()[-2])
-    f.close()
+    if datafile is None:
+        datafiles = glob.glob(os.path.join(dospath,'*{}.dat'.format(tag)))
+        if len(datafiles)>1:
+            print('tell me which file you want using:')
+            for i,f in enumerate(datafiles):
+                print("{}. {}\n".format(i,f))
+            idx = int(input('Type it here:'))
+            datafile = datafiles[idx]
+        elif not datafiles:
+            print('There is no gnu data file!!!')
+            return -1
+        else:
+            datafile = datafiles[0]
+    if fermi is None:
+        with open(datafile,'r') as f:
+            fermi = f.readlines()[0]
+        fermi = float(fermi.split()[-2])
+        f.close()
     dos_data = pd.read_csv(datafile, delimiter='\s+',header=None, skiprows = [0])
     dos_data = dos_data.to_numpy()
     # substract fermi energy level from the results
@@ -300,6 +336,11 @@ def pdosplot(pdospath,ax,dospath=None,nscfpath=None,orbital=None,tag='pdos_atm',
     ax.set_ylabel('PDOS(states/eV)')
     return pdos,fermi
 
+def pdosplot_soc():
+    """
+    Plot pdos from noncolin+soc calculation
+    """
+    pass
 
 if __name__ == "__main__":
     pdospath = 'C://Users//liu_s//Documents//BW_results//SA//Naf1//GS//DOS-PDOS'
