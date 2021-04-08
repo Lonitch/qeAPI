8f531d45d44f4bf96b6df76ebdb3b5dc975a5a68
Lonitch <invictumltd@gmail.com>
2021-04-08 12:11:54 -0500

    update opt_check.py by adding more comments

--- opt_check.py
+++ opt_check.py
@@ -1,10 +1,20 @@
+"""
+This script checks the integrity of QE output files. Any output file ends with 
+the strings in "check" list are considered as incomplete, and files do not end with 
+strings in "check2" list are also considered as incomplete. The code will then delete 
+the input files for completed jobs and leave the input files of incomplete jobs intact.
+"""
+
 import glob,os,sys
 
 keyword = input('Tell me a keyword in output file names:')
 files = glob.glob('*{}*.out'.format(keyword))
-ans = input('Remove slurm-related output file?(Y/N):')
+ans = input('Remove slurm-related output files?(Y/N):')
 
-check = ['convergence NOT achieved','%%%%%%%%%%%%%%%%','campuscluster']
+# Things you don't want to see in complete output files
+check = ['convergence NOT achieved','%%%%%%%%%%%%%%%%']
+# Thins you want to see in complete output files
+check2 = ['JOB DONE.']
 for i,f in enumerate(files):
     rmflg = False
     with open(f,'r') as cc:
@@ -15,6 +25,8 @@ for i,f in enumerate(files):
     else:
         for q in range(1,200):
             if any([_a in content[-q] for _a in check]):
+                rmflg=True
+            if not any([_a in content[-q] for _a in check2]):
                 rmflg=True
 
     if not rmflg:
